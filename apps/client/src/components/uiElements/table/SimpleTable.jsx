8023a0b97a83d04cdfbddf619b7c25c3bda988fa
VictorArdila <valfonsoardila@unicesar.edu.co>
2023-11-17 14:59:14 -0500

    Agregacion de tabla de usuario al modulo home

--- apps/client/src/components/uiElements/table/SimpleTable.jsx
+++ apps/client/src/components/uiElements/table/SimpleTable.jsx
@@ -1,45 +1,33 @@
-import { useReactTable } from '@tanstack/react-table'
-import './SimpleTable.css';
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./SimpleTable.css";
 
 function SimpleTable(prop) {
-    const {data} = prop
-    const columns = [
-        {
-            header: 'ID',
-            accessorKey: 'id'
-        },
-        {
-            header: 'First Name',
-            accessorKey: 'firstName'
-        },
-        {
-            header: 'Last Name',
-            accessorKey: 'lastName'
-        },
-        {
-            header: 'Username',
-            accessorKey: 'username'
-        },
-        {
-            header: 'Password',
-            accessorKey: 'password'
-        },
-        {
-            header: 'Registration Date',
-            accessorKey: 'registrationDate'
-        }
-    ]
+  const { data } = prop;
 
-    const table = useReactTable(data, columns)
   return (
     <div className="container-table">
-      <table>
-        <thead>
-            
-        </thead>
-      </table>
+      <div className="table-wrapper">
+        <div className="table-wrapper-boddy">
+          <AnimatePresence>
+            {data.map((item) => (
+              <motion.div
+                className="user-box"
+                key={item.id}
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.5 }}
+              >
+                <div className="user-name">{item.username}</div>
+                <div className="user-regist">{item.registrationDate}</div>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SimpleTable
+export default SimpleTable;
